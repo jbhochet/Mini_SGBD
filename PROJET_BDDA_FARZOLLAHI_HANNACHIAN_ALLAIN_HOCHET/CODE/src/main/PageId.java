44d568abeb4db4f39da61666dff550722a5a782a
Sepanta007 <sepantaff7900@gmail.com>
2023-09-27 21:14:22 +0200

    Edit PageId.java (adding getters)

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/PageId.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/PageId.java
@@ -1,11 +1,19 @@
 package main;
 
 public class PageId {
-    public int FileIdx;
-    public int PageIdx;
+    private int FileIdx;
+    private int PageIdx;
 
     public PageId(int fileIdx, int pageIdx) {
         this.FileIdx = fileIdx;
         this.PageIdx = pageIdx;
+    }
+
+    public int getFileIdx() {
+        return FileIdx;
+    }
+
+    public int getPageIdx() {
+        return PageIdx;
     }
 }
