513ebf4f5a8419a20bed0bd032b32f6349bc1b05
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-09-27 17:16:17 +0200

    create package main and test in src

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/scripts/compile_run.bash
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/scripts/compile_run.bash
@@ -5,7 +5,7 @@ cd $(dirname ${BASH_SOURCE[0]})
 JAVA_VERSION=17
 SRC_DIR=../src
 BIN_DIR=../bin
-MAIN_CLASS=Main
+MAIN_CLASS=main.Main
 
 # Clear bin directory
 rm -rf $BIN_DIR
@@ -15,7 +15,7 @@ javac \
     --release $JAVA_VERSION \
     --source-path $SRC_DIR \
     -d $BIN_DIR \
-    ${SRC_DIR}/${MAIN_CLASS}.java
+    ${SRC_DIR}/main/Main.java
 
 # Run
 java -cp $BIN_DIR $MAIN_CLASS
