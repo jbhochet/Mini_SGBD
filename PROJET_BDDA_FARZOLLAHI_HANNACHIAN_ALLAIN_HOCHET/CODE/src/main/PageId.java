18b36a8195688de563678e670d0c51e69b8b4b9f
hagop-h <hagob968@gmail.com>
2023-09-27 17:26:52 +0200

    PageId

---
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/PageId.java
@@ -0,0 +1,11 @@
+package main;
+
+public class PageId {
+    public int FileIdx;
+    public int PageIdx;
+
+    public PageId(int fileIdx, int pageIdx) {
+        this.FileIdx = fileIdx;
+        this.PageIdx = pageIdx;
+    }
+}
