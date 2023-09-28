65b18cb99c8e1c81628608f66ca9c32f0f14ddd4
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-09-28 14:37:36 +0200

    fix diskmanager

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/DiskManager.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/DiskManager.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -8,21 +9,19 @@ import java.util.List;
 public class DiskManager {
     private static DiskManager instance;
     private List<File> dataFiles;
-    private int pageSize;
-    private List<PageId> deallocatedPages; 
+    private List<PageId> deallocatedPages;
 
     // Constructor to initialize DBPath
     public DiskManager() {
         this.dataFiles = new ArrayList<>();
-        this.pageSize = DBParams.pageSize;
         this.deallocatedPages = new ArrayList<>();
-        //createDataFiles(DBParams.DMFileCount); 
+        createDataFiles(DBParams.DMFileCount);
     }
 
     // Singleton : getInstance method
     public static DiskManager getInstance() {
         if (instance == null) {
-           
+
             instance = new DiskManager();
         }
         return instance;
@@ -31,7 +30,7 @@ public class DiskManager {
     // Allouer une page
     public PageId AllocPage() {
         PageId allocatedPageId = null;
-        
+
         // Vérifie s'il y a des pages désallouées disponibles.
         if (!deallocatedPages.isEmpty()) {
             // Si oui, allouer la première page désallouée.
@@ -42,7 +41,7 @@ public class DiskManager {
             for (File file : dataFiles) {
                 if (file.length() % pageSize == 0) {
                     if (target == null || file.length() < target.length()) {
-                    	target = file;
+                        target = file;
                     }
                 }
             }
@@ -52,17 +51,18 @@ public class DiskManager {
             }
             // Calculer l'indice de la page allouée dans le fichier cible.
             int pageIdx = (int) (target.length() / pageSize);
-            
+
             // Créer l'identifiant de la page allouée.
             allocatedPageId = new PageId(dataFiles.indexOf(target), pageIdx);
 
             pageIdx++;
-            //ajoute une nouvelle page vide au fichier.
+            // ajoute une nouvelle page vide au fichier.
             if (pageIdx * pageSize >= target.length()) {
                 try {
                     RandomAccessFile randomAccessFile = new RandomAccessFile(target, "rw");
                     randomAccessFile.seek(target.length());
-                    randomAccessFile.write(new byte[pageSize]); // Écrire des données vides pour créer une nouvelle page 4 KO
+                    randomAccessFile.write(new byte[pageSize]); // Écrire des données vides pour créer une nouvelle page
+                                                                // 4 KO
                     randomAccessFile.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -74,38 +74,12 @@ public class DiskManager {
         return allocatedPageId;
     }
 
-
-
-    /*
     public void ReadPage(PageId pageId, byte[] buff) {
-        String fileName = DBPath + File.separator + "F" + pageId.getFileIdx() + ".data";
-        int offset = pageId.getPageIdx() * pageSize;
+    }
 
-        try {
-            RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "r");
-            randomAccessFile.seek(offset);
-
-            int bytesRead = randomAccessFile.read(buff);
-            randomAccessFile.close();
-
-            if (bytesRead == -1) {
-                System.err.println("End of file.");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Error reading data.");
-        }
-    }*/
-    
-    /*
-    //public void ReadPage(PageId pageId, byte[] buff)
-    
-   
-     */
-    /*
     private void createDataFiles(int fileCount) {
         for (int i = 1; i <= fileCount; i++) {
-            String fileName = DBPath + File.separator + "F" + i + ".data";
+            String fileName = DBParams.DBPath + File.separator + "F" + i + ".data";
             File file = new File(fileName);
             try {
                 file.createNewFile();
@@ -117,10 +91,12 @@ public class DiskManager {
             }
         }
     }
-    */
-    
-    //public void DeallocPage(PageId pageId) {}
-    
-    //public int GetCurrenctCountAllocPages(){}
+
+    public void DeallocPage(PageId pageId) {
+    }
+
+    public int GetCurrenctCountAllocPages() {
+        return 0;
+    }
 
 }
