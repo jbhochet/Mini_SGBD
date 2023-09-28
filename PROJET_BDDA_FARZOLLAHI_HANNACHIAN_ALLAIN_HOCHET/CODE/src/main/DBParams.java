53ac401c6b22465843b11c3e8bb0ae7890f367d0
Yanis Allain <yanis.allain@etu.u-paris.fr>
2023-09-28 14:04:43 +0200

    Fix DBParams class

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/DBParams.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/DBParams.java
@@ -1,31 +1,8 @@
 package main;
+
 public class DBParams {
-    private static String DBPath;
-    private static int SGBDPageSize;
-    private static int DMFileCount;
-
-    public static String getDBPath(){
-        return DBPath;
-    }
-
-    public static void setDBPath(String path){
-        DBPath=path;
-    }
-
-    public static int getSGBDPageSize(){
-        return SGBDPageSize;
-    }
-
-    public static void setSGBDPageSize(int size){
-        SGBDPageSize=size;
-    }
-
-    public static int getDMFileCount(){
-        return DMFileCount;
-    }
-
-    public static void setDMFileCount(int count){
-        DMFileCount=count;
-    }
+    public static String DBPath;
+    public static int SGBDPageSize;
+    public static int DMFileCount;
 
 }
