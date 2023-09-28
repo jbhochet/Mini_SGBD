d3893dc902a2fb8a12f2d53a06120360757b68b9
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-09-28 14:12:40 +0200

    fix Main class

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/Main.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/Main.java
@@ -3,6 +3,8 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DBParams.DBPath = (args.length == 0) ? "../DB" : args[0];
+        DBParams.SGBDPageSize = 4096;
+        DBParams.DMFileCount = 4;
     }
 }
