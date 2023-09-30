b8a660d4b195eb896e823117febbc6aff77a6713
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-09-30 17:21:41 +0200

    setup junit

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/scripts/compile_run.bash
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/scripts/compile_run.bash
@@ -1,3 +1,5 @@
+DB_PATH=$(realpath $1)
+
 # The script must work every time
 cd $(dirname ${BASH_SOURCE[0]})
 
@@ -5,7 +7,9 @@ cd $(dirname ${BASH_SOURCE[0]})
 JAVA_VERSION=17
 SRC_DIR=../src
 BIN_DIR=../bin
+LIB_DIR=../lib
 MAIN_CLASS=main.Main
+SOURCES_FILES=$(find $SRC_DIR -name "*.java")
 
 # Clear bin directory
 rm -rf $BIN_DIR
@@ -14,8 +18,17 @@ rm -rf $BIN_DIR
 javac \
     --release $JAVA_VERSION \
     --source-path $SRC_DIR \
+    -cp ${LIB_DIR}/*.jar \
     -d $BIN_DIR \
-    ${SRC_DIR}/main/Main.java
+    $SOURCES_FILES
+
+# Run tests
+java -jar $LIB_DIR/junit-platform-console-standalone-1.9.3.jar -cp $BIN_DIR --scan-classpath
+
+if [ $? != 0 ]; then
+    echo "Unit test failure!"
+    exit 1
+fi
 
 # Run
-java -cp $BIN_DIR $MAIN_CLASS
+java -cp $BIN_DIR $MAIN_CLASS $DB_PATH
