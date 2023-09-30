2636b785b55be6b0a18cd78b2f1b76732d87a326
hagop-h <hagob968@gmail.com>
2023-09-30 17:06:29 +0200

    Adding testUnit for all the methodes of DiskManager TD2

---
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/test/DiskManagerTest.java
@@ -0,0 +1,79 @@
+package JUnit_Test;
+
+import org.junit.jupiter.api.Test;
+
+import Main.PageId;
+import Main.DBParams;
+import Main.DiskManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+
+public class DiskManagerTest {
+    DiskManager diskManager;
+    
+    @Test
+    public void testAllocPage() throws IOException {
+        String dbPath =  "../DB";
+        DBParams.DBPath = dbPath;
+        DBParams.DMFileCount = 4;
+        DBParams.SGBDPageSize = 4096;
+        diskManager = DiskManager.getInstance();
+
+		PageId allocatedPageId = diskManager.AllocPage();
+
+		assertNotNull(allocatedPageId);
+		assertEquals(1, allocatedPageId.getPageIdx());
+    }
+    
+    @Test
+    public void testReadWritePage() {
+        try {
+        
+        	String dbPath = "../DB";
+            DBParams.DBPath = dbPath;
+            DBParams.DMFileCount = 4;
+            DBParams.SGBDPageSize = 4096;
+            diskManager = DiskManager.getInstance();
+
+            PageId allocatedPageId = diskManager.AllocPage();
+
+            ByteBuffer writeBuffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+            writeBuffer.putInt(42); 
+            writeBuffer.flip();
+
+            diskManager.WritePage(allocatedPageId, writeBuffer);
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+
+            diskManager.ReadPage(allocatedPageId, readBuffer);
+
+            int data = readBuffer.getInt();
+            assertEquals(42, data);
+        } catch (IOException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+    
+    @Test
+    public void testDeallocPage() throws IOException {
+    	 String dbPath =  "../DB";
+         DBParams.DBPath = dbPath;
+         DBParams.DMFileCount = 4;
+         DBParams.SGBDPageSize = 4096;
+         diskManager = DiskManager.getInstance();
+
+         PageId allocatedPageId = diskManager.AllocPage();
+        
+         diskManager.DeallocPage(allocatedPageId);
+
+         int allocatedPagesCount = diskManager.GetCurrentCountAllocPages();
+         assertEquals(2, allocatedPagesCount);
+        
+    }
+    
+}
+
