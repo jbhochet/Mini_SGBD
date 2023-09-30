68ed0e997c9b5b94634c6e4bfcaf50d8eb93be84
hagop-h <hagob968@gmail.com>
2023-09-30 17:28:30 +0200

    Added Test only for the methode Alloc()

---
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/test/AllocTest.java
@@ -0,0 +1,33 @@
+package JUnit_Test;
+
+import org.junit.jupiter.api.Test;
+
+import Main.PageId;
+import Main.DBParams;
+import Main.DiskManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+public class AllocTest {
+    private DiskManager diskManager;
+
+    @Test
+    public void testAllocPage() throws IOException {
+        String dbPath = "../DB";
+        DBParams.DBPath = dbPath;
+        DBParams.DMFileCount = 1;
+        DBParams.SGBDPageSize = 4096;
+        diskManager = DiskManager.getInstance();
+
+        int numberOfPagesToAllocate = 100;
+
+        for (int i = 1; i < numberOfPagesToAllocate; i++) {
+            PageId allocatedPageId = diskManager.AllocPage();
+
+            assertNotNull(allocatedPageId);
+            assertEquals(i, allocatedPageId.getPageIdx());
+        }
+    }
+}
