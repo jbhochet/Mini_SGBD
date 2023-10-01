d88a69fbc6a754691a01e59c410215d0324a9a8d
hagop-h <hagob968@gmail.com>
2023-10-01 19:40:38 +0200

    fixing some issues

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/scripts/compile_and_run.bat
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/scripts/compile_and_run.bat
@@ -1,23 +1,33 @@
+
 @echo off
-:: The script directory
-setlocal enabledelayedexpansion
-for %%i in (%~dp0) do set "SCRIPT_DIR=%%~fi"
+setlocal
 
-:: Configuration
-set "JAVA_VERSION=17"
-set "SRC_DIR=..\src"
-set "BIN_DIR=..\bin"
-set "MAIN_CLASS=main.Main"
+rem Configuration
+set JAVA_VERSION=17
+set SRC_DIR=src
+set BIN_DIR=bin
+set LIB_DIR=lib
+set MAIN_CLASS=main.Main
+set DB_PATH=%~dp1
 
-:: Additional Argument
-set "ARGUMENT=../DB"
+rem The script must work every time
+cd /d "%~dp0\.."
 
-:: Clear bin directory
-rmdir /s /q "%BIN_DIR%"
+rem Clear bin directory
+rmdir /s /q "%BIN_DIR%" 2>nul
 mkdir "%BIN_DIR%"
 
-:: Compile
-javac --release !JAVA_VERSION! --source-path "%SRC_DIR%" -d "%BIN_DIR%" "%SRC_DIR%\main\Main.java"
+rem Compile
+javac --release %JAVA_VERSION% --source-path %SRC_DIR% -cp "%LIB_DIR%\*" -d %BIN_DIR% %SRC_DIR%\*.java
 
-:: Run with Argument
-java -cp "%BIN_DIR%" %MAIN_CLASS% "%ARGUMENT%"
+rem Run tests
+java -jar "%LIB_DIR%\junit-platform-console-standalone-1.9.3.jar" -cp %BIN_DIR% --scan-classpath
+
+if %errorlevel% neq 0 (
+    set /p continue=Unit test failure! Continue? (press enter for yes, C-c to exit)
+)
+
+rem Run
+java -cp %BIN_DIR% %MAIN_CLASS% %DB_PATH%
+
+endlocal
