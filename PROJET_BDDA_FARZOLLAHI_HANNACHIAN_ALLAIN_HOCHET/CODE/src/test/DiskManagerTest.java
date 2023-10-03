741193bd4a1faa566790ea07e8e10c3020b66112
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-10-03 21:02:33 +0200

    rework and improve tests

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/test/DiskManagerTest.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/test/DiskManagerTest.java
@@ -1,79 +1,106 @@
 package test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
 import main.PageId;
 import main.DBParams;
 import main.DiskManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-
+import java.nio.file.Files;
+import java.util.Stack;
 
 public class DiskManagerTest {
-    DiskManager diskManager;
-    
-    @Test
-    public void testAllocPage() throws IOException {
-        String dbPath =  "../DB";
-        DBParams.DBPath = dbPath;
+    private static File tmpDir;
+    private static DiskManager diskManager;
+
+    @BeforeAll
+    public static void setup() throws IOException {
+        tmpDir = Files.createTempDirectory("DB_").toFile();
+        DBParams.DBPath = tmpDir.getAbsolutePath();
         DBParams.DMFileCount = 4;
         DBParams.SGBDPageSize = 4096;
-        diskManager = DiskManager.getInstance();
-
-		PageId allocatedPageId = diskManager.AllocPage();
-
-		assertNotNull(allocatedPageId);
-		assertEquals(0, allocatedPageId.getPageIdx());
-    }
-    
-    @Test
-    public void testReadWritePage() {
         try {
-        
-        	String dbPath = "../DB";
-            DBParams.DBPath = dbPath;
-            DBParams.DMFileCount = 4;
-            DBParams.SGBDPageSize = 4096;
             diskManager = DiskManager.getInstance();
-
-            PageId allocatedPageId = diskManager.AllocPage();
-
-            ByteBuffer writeBuffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
-            writeBuffer.putInt(42); 
-            writeBuffer.flip();
-
-            diskManager.WritePage(allocatedPageId, writeBuffer);
-
-            ByteBuffer readBuffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
-
-            diskManager.ReadPage(allocatedPageId, readBuffer);
-
-            int data = readBuffer.getInt();
-            assertEquals(42, data);
         } catch (IOException e) {
-            fail("Exception thrown: " + e.getMessage());
+            System.out.println(e);
         }
     }
-    
-    @Test
-    public void testDeallocPage() throws IOException {
-    	 String dbPath =  "../DB";
-         DBParams.DBPath = dbPath;
-         DBParams.DMFileCount = 4;
-         DBParams.SGBDPageSize = 4096;
-         diskManager = DiskManager.getInstance();
 
-         PageId allocatedPageId = diskManager.AllocPage();
-        
-         diskManager.DeallocPage(allocatedPageId);
-
-         int allocatedPagesCount = diskManager.GetCurrentCountAllocPages();
-         assertEquals(2, allocatedPagesCount);
-        
+    @AfterAll
+    public static void exit() throws IOException {
+        for (File file : tmpDir.listFiles())
+            file.delete();
+        tmpDir.delete();
     }
-    
-}
 
+    /*********
+     * Tests *
+     *********/
+
+    @Test
+    @Order(1)
+    public void testAllocAndDealloc() throws IOException {
+        final int MAX = 100;
+        final Stack<PageId> stack = new Stack<>();
+        for (int i = 0; i < MAX; i++) {
+            stack.add(diskManager.AllocPage());
+            assertEquals(i + 1, diskManager.GetCurrentCountAllocPages());
+        }
+        for (int i = 0; i < MAX; i++) {
+            diskManager.DeallocPage(stack.pop());
+            assertEquals(MAX - i - 1, diskManager.GetCurrentCountAllocPages());
+        }
+    }
+
+    @Test
+    @Order(2)
+    public void testReadAndWrite() throws IOException {
+        final int MAX = 100;
+        Stack<PageId> stack = new Stack<>();
+        for (int i = 0; i < MAX; i++) {
+            PageId pageId = diskManager.AllocPage();
+
+            ByteBuffer writeBuffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+            writeBuffer.putInt(i);
+            writeBuffer.flip();
+            diskManager.WritePage(pageId, writeBuffer);
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+            diskManager.ReadPage(pageId, readBuffer);
+            int data = readBuffer.getInt();
+
+            assertEquals(i, data);
+
+            stack.add(pageId);
+        }
+        while (!stack.empty())
+            diskManager.DeallocPage(stack.pop());
+    }
+
+    @Test
+    @Order(3)
+    public void testWriteAndReadString() throws IOException {
+        String phrase = "We are the best, We are BDDA";
+        PageId pageId = diskManager.AllocPage();
+        ByteBuffer tamponPhrase = ByteBuffer.allocate(DBParams.SGBDPageSize);
+        tamponPhrase.put(phrase.getBytes());
+        tamponPhrase.flip();
+        diskManager.WritePage(pageId, tamponPhrase);
+
+        ByteBuffer tamponLuPhrase = ByteBuffer.allocate(DBParams.SGBDPageSize);
+        diskManager.ReadPage(pageId, tamponLuPhrase);
+        String phraseLue = new String(tamponLuPhrase.array()).trim();
+
+        assertEquals(phrase, phraseLue);
+
+        diskManager.DeallocPage(pageId);
+    }
+
+}
