afa9a3c57aaf72c0021a3fd7a318ff0057765d64
Sepanta007 <sepantaff7900@gmail.com>
2023-10-12 14:20:12 +0200

    Edit CollInfo.java

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/ColInfo.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/ColInfo.java
@@ -3,12 +3,17 @@ package main;
 class ColInfo {
     private String colName;
     private String colType;
+    private int T;
 
     public ColInfo(String colName, String colType) {
         this.colName = colName;
         this.colType = colType;
     }
 
+    public ColInfo(String colName, String colType, int T) {
+        this(colName, colType);
+        this.T = T;
+    }
 
     public String getColName() {
         return colName;
@@ -28,5 +33,13 @@ class ColInfo {
 
     public boolean isValidColumnType(String type) {
         return type.equals("INT") || type.equals("FLOAT") || type.matches("STRING\\(\\d+\\)") || type.matches("VARSTRING\\(\\d+\\)");
+    }
+
+    public int getT() {
+        return T;
+    }
+
+    public void setT(int t) {
+        T = t;
     }
 }
