3cedc6d943bdbdda22c1013297f95afe3a3e8a2c
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-11-09 12:40:57 +0100

    update shell.nix and .gitignore

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/shell.nix
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
-  nativeBuildInputs = with pkgs.buildPackages; [ jdk ];
+  nativeBuildInputs = with pkgs.buildPackages; [ jdk17 python3 ];
 }
