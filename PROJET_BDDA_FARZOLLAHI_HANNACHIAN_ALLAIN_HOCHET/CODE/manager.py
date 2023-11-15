f7d85db9dfc4d23191e972b0fc24cf055adebb0f
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-11-15 16:35:01 +0100

    remove junit

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/manager.py
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/manager.py
@@ -44,8 +44,6 @@ def compileJava(src, bin, classpath, version):
 JAVA_VERSION = 17
 SOURCE_DIR = "src"
 BIN_DIR = "bin"
-LIB_DIR = "lib"
-JUNIT_JAR = "junit-platform-console-standalone-1.9.3.jar"
 MAIN_CLASS = "Main"
 
 # Setup command line argument
@@ -54,9 +52,14 @@ parser = argparse.ArgumentParser(description="SGBD Manager (◠﹏◠)")
 
 parser.add_argument("dbpath", help="The path to the DB folder")
 
+parser.add_argument("--main-class", help="Run this class instead of the default.")
+
 parser.add_argument("--test", action="store_true", help="Add this flag for compile and run Junit tests")
 
 args = parser.parse_args()
+
+if args.main_class is not None:
+    MAIN_CLASS = args.main_class
 
 dbpath = path.realpath(args.dbpath)
 
@@ -71,12 +74,7 @@ compileJava(path.join(SOURCE_DIR, "main"), BIN_DIR, [], JAVA_VERSION)
 
 # Compile tests files and run junit
 if(args.test):
-    libs = listDir(LIB_DIR, lambda f: f.endswith(".jar"), [])
-    compileJava(path.join(SOURCE_DIR, "test"), BIN_DIR, libs + [BIN_DIR], JAVA_VERSION)
-    result = subprocess.run(["java", "-jar", path.join(LIB_DIR, JUNIT_JAR), "-cp", BIN_DIR, "--scan-classpath"])
-    if result.returncode != 0:
-        if(input("Do you want to continue? [Y] ") != 'Y'):
-            exit(0)
+    compileJava(path.join(SOURCE_DIR, "test"), BIN_DIR, [BIN_DIR], JAVA_VERSION)
 
 # Run java code
 subprocess.run(["java", "-cp", BIN_DIR, MAIN_CLASS, dbpath])
