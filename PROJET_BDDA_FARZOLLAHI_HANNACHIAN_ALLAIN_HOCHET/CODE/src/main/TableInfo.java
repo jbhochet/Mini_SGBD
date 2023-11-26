b99540299e9a73f95d1275a9313966477434fdbc
hagop-h <hagob968@gmail.com>
2023-11-26 21:18:36 +0100

    fixing a bug

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/TableInfo.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/TableInfo.java
@@ -26,5 +26,15 @@ public class TableInfo implements Serializable {
     public PageId getHeaderPageId() {
         return headerPageId;
     }
+	
+    public int getColumnIndex(String columnName) {
+        for (int i = 0; i < columns.length; i++) {
+            if (columns[i].getName().equals(columnName)) {
+                return i;
+            }
+        }
+        // Handle the case where the column name is not found
+        throw new IllegalArgumentException("Column not found: " + columnName);
+    }
 
 }
