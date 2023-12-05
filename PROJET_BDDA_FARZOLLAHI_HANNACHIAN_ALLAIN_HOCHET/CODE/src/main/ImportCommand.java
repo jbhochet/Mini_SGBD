aa43502682921921d5a8072b1cb80ee7e6d3ecf3
Jb Hochet <jean-baptiste.hochet@proton.me>
2023-12-05 10:04:34 +0100

    create ImportCommand

--- PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/ImportCommand.java
+++ PROJET_BDDA_FARZOLLAHI_HANNACHIAN_ALLAIN_HOCHET/CODE/src/main/ImportCommand.java
@@ -1,46 +1,38 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ImportCommand implements ICommand {
+    public static final Pattern PATTERN = Pattern.compile("^IMPORT INTO (\\w+) (\\w+.csv)$");
+    public static final String DELIM = ",";
 
-    private String relationName;
-    private String fileName;
+    private String relation;
+    private File file;
 
     public ImportCommand(String command) {
-        parseCommand(command);
-    }
-
-    private void parseCommand(String command) {
-        String[] tokens = command.split("\\s+");
-        this.relationName = tokens[2];
-        this.fileName = tokens[3];
+        Matcher matcher = PATTERN.matcher(command);
+        if (!matcher.matches())
+            throw new IllegalArgumentException("This command does not match IMPORT pattern!");
+        this.relation = matcher.group(1);
+        this.file = new File(matcher.group(2));
     }
 
     @Override
     public void execute() throws IOException {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line;
+        TableInfo tableInfo = DatabaseInfo.getInstance().getTableInfo(this.relation);
+        FileManager fileManager = FileManager.getInstance();
+        Record record;
+        String line;
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.file))) {
             while ((line = reader.readLine()) != null) {
-                String[] values = line.split("\\t");
-                Record record = new Record(DatabaseInfo.getInstance().getTableInfo(relationName));
-
-                /*
-                for (String value : values) {
-                    record.addRecordValue(value);
-                }
-                 */
-
-                System.out.println("Inserting record into " + relationName + ": " + line);
+                record = new Record(tableInfo);
+                record.setRecValues(line.split(DELIM));
+                fileManager.InsertRecordIntoTable(record);
             }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new IOException("Erreur lors de l'importation : " + e.getMessage());
         }
     }
 }
-
